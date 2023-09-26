db4fd2bf09ef201f887478b57aa24fd25f1491b1
Davis.Nguyen <davis.nguyen@avepoint.com>
2023-09-26 16:04:40 +0700

    first commit

---
+++ test/ProductQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace test
+{
+    public class ProductQuery : DbAccess
+    {
+        public ProductQuery(string connectionString) : base(connectionString) { }
+
+        public DataSet FindProduct(string productName)
+        {
+            DataSet dataSet = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Products WHERE ProductName = @ProductName";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ProductName", productName);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataSet);
+            }
+
+            return dataSet;
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("input product name:");
+                var productName = Console.ReadLine();
+
+
+                string store1ConnectionString = ConfigurationManager.ConnectionStrings["StoreA"].ConnectionString;
+                string store2ConnectionString = ConfigurationManager.ConnectionStrings["StoreB"].ConnectionString;
+
+                ProductQuery queryStore1 = new ProductQuery(store1ConnectionString);
+                ProductQuery queryStore2 = new ProductQuery(store2ConnectionString);
+
+                DataSet dataSetStore1 = queryStore1.FindProduct(productName);
+                DataSet dataSetStore2 = queryStore2.FindProduct(productName);
+
+                bool productAvailableInBothStores = (dataSetStore1.Tables[0].Rows.Count > 0) && (dataSetStore2.Tables[0].Rows.Count > 0);
+
+                if (productAvailableInBothStores)
+                {
+                    Console.WriteLine("The product is available in both stores.");
+                }
+                else
+                {
+                    Console.WriteLine("The product is not available in both stores.");
+                }
+
+                Console.WriteLine("do you want to continue? (yes/no)");
+                var isContinue = Console.ReadLine();
+                if (isContinue != "yes")
+                {
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
